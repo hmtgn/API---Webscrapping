a761da990fa632ff20408c3e58a7a3fdaf051498
hmtgn <hugo.montagnon@epfedu.fr>
2024-12-16 12:14:20 +0100

    step 6 - Manage Dataset

--- TP2 and  3/services/epf-flower-data-science/src/api/router.py
+++ TP2 and  3/services/epf-flower-data-science/src/api/router.py
@@ -1,8 +1,10 @@
 """API Router for Fast API."""
 from fastapi import APIRouter
-
+from src.api.routes import data
 from src.api.routes import hello
 
 router = APIRouter()
 
 router.include_router(hello.router, tags=["Hello"])
+router.include_router(data.router, prefix="/api")
+
