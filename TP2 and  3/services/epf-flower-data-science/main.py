6d4f1e710140f276cbc7bc045d06e241ea60f607
hmtgn <hugo.montagnon@epfedu.fr>
2024-12-21 16:29:43 +0100

    Version v1 deploy

--- TP2 and  3/services/epf-flower-data-science/main.py
+++ TP2 and  3/services/epf-flower-data-science/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from src.app import get_application
 from src.api.routes import data
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 
 app = get_application()
 
@@ -11,6 +14,15 @@ app = get_application()
 async def redirect_to_docs():
     return RedirectResponse(url="/docs")
 
+# Custom error handler for 404
+@app.exception_handler(404)
+async def custom_404_handler(request: Request, exc):
+    return JSONResponse(
+        status_code=404,
+        content={"error": "The requested resource was not found on this server."}
+    )
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", reload=True, port=8080)
+
--- TP2 and  3/services/epf-flower-data-science/src/api/router.py
+++ TP2 and  3/services/epf-flower-data-science/src/api/router.py
@@ -6,5 +6,5 @@ from src.api.routes import hello
 router = APIRouter()
 
 router.include_router(hello.router, tags=["Hello"])
-router.include_router(data.router, prefix="/api")
+router.include_router(data.router, prefix="/v1")
 
